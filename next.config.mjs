bdb22c494458c82f2b36a6ff4092c97af41b03d2
prkty <prkty0@gmail.com>
2025-07-04 22:57:45 +0900

    webpack 관련 서버 502 문제 핫픽스2

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,13 @@ const nextConfig = {
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV || 'development',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        "@": process.cwd(), // __dirname이 아니라 process.cwd()를 써야 Next.js에서 루트로 인식
+      };
+    return config;
+  },
 }
 
 export default nextConfig
